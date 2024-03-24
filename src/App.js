1756cf249d7748b6607786d66c11de6154c42bea
SokhnaDIOPD22011515 <smdbdiop@gmail.com>
2024-03-24 12:14:00 +0100

    Ajout du changement d'ordre des tâches

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import "./styles.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import arrowUp from "./img/arrow-up.png";
+import arrowDown from "./img/arrow-down.png";
 
 export default function App() {
   const [items, setItems] = useState([
@@ -50,6 +52,28 @@ export default function App() {
       task.text.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
+  function moveTaskUp(index) {
+    if (index > 0) {
+      const taskIndex = items.findIndex(task => task.id === filteredTasks[index].id);
+      const updatedItems = [...items];
+      const temp = updatedItems[taskIndex];
+      updatedItems[taskIndex] = updatedItems[taskIndex - 1];
+      updatedItems[taskIndex - 1] = temp;
+      setItems(updatedItems);
+    }
+  }
+
+  function moveTaskDown(index) {
+    if (index < filteredTasks.length - 1) {
+      const taskIndex = items.findIndex(task => task.id === filteredTasks[index].id);
+      const updatedItems = [...items];
+      const temp = updatedItems[taskIndex];
+      updatedItems[taskIndex] = updatedItems[taskIndex + 1];
+      updatedItems[taskIndex + 1] = temp;
+      setItems(updatedItems);
+    }
+  }
+
   return (
       <div id="app">
           <Header itemsCount={items.length}
@@ -58,24 +82,28 @@ export default function App() {
           <ol>
               {filteredTasks.map((item, index) => (
                   <li key={index}>
-                      <label>
-                          <input
-                              type="checkbox"
-                              checked={item.done}
-                              onChange={() => handleCheckboxClicked(item.id)}
-                          />
-                          <span className={item.done ? "done" : ""}>{item.text}</span>
-                          <button className="delete" onClick={() => handleDelete(item.id)}>
-                              Supprimer
-                          </button>
-                      </label>
+                    <label>
+                      <div id="arrows">
+                        <button className="arrows" onClick={() => moveTaskUp(index)}><img src={arrowUp} alt="Flèche vers le haut"/></button>
+                        <button className="arrows" onClick={() => moveTaskDown(index)}><img src={arrowDown} alt="Flèche vers le bas"/></button>
+                      </div>
+                      <input
+                          type="checkbox"
+                          checked={item.done}
+                          onChange={() => handleCheckboxClicked(item.id)}
+                      />
+                      <span className={item.done ? "done" : ""}>{item.text}</span>
+                      <button className="delete" onClick={() => handleDelete(item.id)}>
+                        Supprimer
+                      </button>
+                    </label>
                   </li>
               ))}
           </ol>
-          <div>
-          </div>
-          <Footer onSearchKeyUp={searchTask}
-                  newTask={newTask}
+        <div>
+        </div>
+        <Footer onSearchKeyUp={searchTask}
+                newTask={newTask}
                   onAddTask={addTask}
                   onNewTaskChange={(e) => setNewTask(e.target.value)}/>
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 export default function Header({itemsCount, itemsDone, itemsToDo}){
     return (
         <>
-            <h2>To Dos :</h2>
+            <h2>To Do</h2>
             <h3>Il y'a {itemsCount} tâches au total</h3>
             <span>
               Il y a <b>{itemsDone}</b> tâches <em id="done">achevées</em> et{" "}
